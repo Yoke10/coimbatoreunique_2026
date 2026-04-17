db089299f461f823eca697e107d1b08212b2777c
Yoke10 <yokesshk@gmail.com>
2026-04-17 13:20:20 +0530

    video section ratio changes

--- src/components/home/PrayerAndTestSection.jsx
+++ src/components/home/PrayerAndTestSection.jsx
@@ -5,7 +5,7 @@ const PrayerAndTestSection = () => {
     return (
         <section className="prayer-test-section section">
             <div className="container">
-                <h2 className="section-main-title">Rotaract Prayer & 4-Way Testt</h2>
+                <h2 className="section-main-title">Rotaract Prayer & 4-Way Test</h2>
                 <div className="container">
                     {/* Prayer Card */}
                     <div className="box">
